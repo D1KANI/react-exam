9b8c8c16b554c727abd9e8e52bd6f6749368d462
Daniil Rybakov <daniil18rusxxx@gmail.com>
2024-06-20 10:54:35 +0400

    :sparkles: FEATURE(PWA): Adding PWA without offline

---
+++ pwaManifest.ts
@@ -0,0 +1,48 @@
+import type { VitePWAOptions } from "vite-plugin-pwa";
+
+export const vitePwaConfig: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: [
+    "/favicon/apple-touch-icon.png",
+    "/favicon/safari-pinned-tab.svg",
+  ],
+  manifest: {
+    name: "Task manager by DIKANI",
+    short_name: "Task manager",
+    description: "Task manager by DIKANI. Save your time.",
+    icons: [
+      {
+        src: "/favicon/favicon-16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/favicon-32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/favicon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/favicon/favicon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/mstile-150.png",
+        sizes: "150x150",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#333333",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { vitePwaConfig } from "./pwaManifest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(vitePwaConfig)],
   server: {
     port: 3000,
   },
